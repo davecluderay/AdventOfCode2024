41ed1756e2627daec87dbe12b1e8f0e51bced49c
Dave Cluderay <davecluderay@users.noreply.github.com>
2024-12-09 22:56:05 +0000

    Create empty solution.

---
+++ Aoc2024-Day01/Solution.cs
@@ -0,0 +1,18 @@
+namespace Aoc2024_Day01;
+
+internal class Solution
+{
+    public string Title => "";
+
+    public object PartOne()
+    {
+        var lines = InputFile.ReadAllLines();
+        return null!;
+    }
+
+    public object PartTwo()
+    {
+        var lines = InputFile.ReadAllLines();
+        return null!;
+    }
+}
