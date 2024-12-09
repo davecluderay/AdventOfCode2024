575442bd43435530881a11b5be6183f7e59b7b2e
Dave Cluderay <davecluderay@users.noreply.github.com>
2024-12-09 23:27:58 +0000

    Add day 1 solution.

--- Aoc2024-Day01/Solution.cs
+++ Aoc2024-Day01/Solution.cs
@@ -1,18 +1,38 @@
+using System.Text.RegularExpressions;
+
 namespace Aoc2024_Day01;
 
-internal class Solution
+internal partial class Solution
 {
-    public string Title => "";
+    public string Title => "Day 1: Historian Hysteria";
 
     public object PartOne()
     {
-        var lines = InputFile.ReadAllLines();
-        return null!;
+        var (left, right) = ReadInputLists();
+        return left.Order()
+                   .Zip(right.Order())
+                   .Sum(x => Math.Abs(x.First - x.Second));
     }
 
     public object PartTwo()
     {
-        var lines = InputFile.ReadAllLines();
-        return null!;
+        var (left, right) = ReadInputLists();
+        return left.Sum(x => x * right.Count(y => y == x));
     }
+
+    private (List<int> Left, List<int> Right) ReadInputLists()
+    {
+        List<int> left = [], right = [];
+        foreach (var line in InputFile.ReadAllLines())
+        {
+            var match = InputPattern.Match(line);
+            if (!match.Success) continue;
+            left.Add(Convert.ToInt32(match.Groups["Left"].Value));
+            right.Add(Convert.ToInt32(match.Groups["Right"].Value));
+        }
+        return (left, right);
+    }
+
+    [GeneratedRegex(@"^(?<Left>\d+)\s+(?<Right>\d+)$", RegexOptions.ExplicitCapture)]
+    private partial Regex InputPattern { get; }
 }
